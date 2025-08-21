6c961490b8ed58a45c681f3f78c9a17497f3e398
Christoph Sven Funke <csfunke@yahoo.com>
2025-08-21 15:52:41 +0200

    Problem in solution loader TimeStepType
    Producing error message in test_1a. Unclear to me how to fix at the moment.

--- tests/testcases/run_test.py
+++ tests/testcases/run_test.py
@@ -584,4 +584,4 @@ if __name__ == "__main__":
 
     config.solver.keep_files = False
     folder_path = os.path.dirname(__file__)
-    test_1j(config, folder_path)
+    test_1a(config, folder_path)
